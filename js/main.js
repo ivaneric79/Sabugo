8bbad57084ed73269001f7439dd817cac421199e
ivaneric79 <ivaneric79@gmail.com>
2013-07-04 17:45:42 -0500

    COMIT

---
+++ js/main.js
@@ -0,0 +1,14 @@
+
+   $(document).ready(function($) {
+  $('#slider').royalSlider({
+   autoScaleSlider: false,
+   imageScaleMode: 'none',
+    arrowsNav: true,
+   controlsInside: true,
+   arrowsNavAutoHide: false
+  
+
+
+
+  });
+});
